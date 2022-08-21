bd9c2d2f0cde34aef287a04eeeb849a3a02bc21d
Vitalik Kolesnikov <VitaliKolesnikov22@gmail.com>
2022-08-21 10:42:06 +0300

    Добавил переменные

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 
 
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 35000
+let income = 'Фриланс'
+let addExpenses = 'Интернет, такси, коммуналка'
+let deposit = true
+let mission = 500000
+let period = 12
+let budgetDay = money / 30
 
 alert('hello world!')
 
 console.log('hello world!');
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+console.log('Период равен' + period + 'месяцев');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(','));
+console.log(Math.floor(budgetDay));
+
 
